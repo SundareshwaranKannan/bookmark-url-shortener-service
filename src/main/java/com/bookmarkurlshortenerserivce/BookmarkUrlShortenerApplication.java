d4e7abafa6fda930ab6b22eed18736432c4e1759
Sundar <37539577+SundareshwaranKannan@users.noreply.github.com>
2020-09-06 16:23:41 +0530

    feat(bookmark-service): initial commit

---
+++ src/main/java/com/bookmarkurlshortenerserivce/BookmarkUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.bookmarkurlshortenerserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookmarkurlshortenerserivce/BookmarkUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarkurlshortenerserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
